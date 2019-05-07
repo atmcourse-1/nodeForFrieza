29e54ac13c3716de39c149af34dfff128455c136
xuelongbai <windowsxuelongbai@gmail.com>
2019-05-07 15:56:22 +0800

    new

--- server.js
+++ server.js
@@ -7,8 +7,14 @@ let bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-const routes = require('./api/routes/todoListRoutes.js')
-routes(app)
+// const routes = require('./api/routes/todoListRoutes.js')
+// routes(app)
+const router = express.Router()
+router.get('/', function(req, res) {
+  res.json({ message: 'API works' })
+})
+
+app.use('/api', router)
 
 app.listen(port)
 
